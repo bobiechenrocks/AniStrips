b833092225a821467b79a44ceebf0a19db715fed
Bobie <bobie@herxun.co>
2014-03-18 17:39:11 +0800

    Initial Commit

---
+++ AniStripes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AniStripes
+//
+//  Created by Bobie Chen on 2014/3/18.
+//  Copyright (c) 2014年 Bobie Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AniStripes/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AniStripes
+//
+//  Created by Bobie Chen on 2014/3/18.
+//  Copyright (c) 2014年 Bobie Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
